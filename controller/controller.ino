053d588eeced29f85b3ff5b3558e10a83c7b5fae
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2022-11-17 17:14:45 -0800

    Implement aiming and pnuematic control

--- controller/controller.ino
+++ controller/controller.ino
@@ -4,8 +4,8 @@
 #define PUL 9
 #define DIR 10
 #define ENA 11
-int dirBin = 0; // 0 is clockwise, 1 is counterclockwise
-int pwmWidth = 300;
+int dirBin = 1; // 0 is clockwise, 1 is counterclockwise
+int pwmWidth = 2000;
 const int minPWMWidth = 40;
 const int maxPWMWidth = 10000;
 
@@ -50,18 +50,127 @@ void setup() {
   srv.attach(SERVOPIN);
 
   unlockServo();
+
+  //
+
+  pinMode(MCROSWC, INPUT);
+
+  //
+
+  pinMode(PSTA, OUTPUT);
+  pinMode(PSTB, OUTPUT);
+
+  resetPiston();
 }
 
 
 ///
-// 150 vs 40
+  /*dirBin = getSwitchPressed();
+  updateDirection();*/
+   /*if (dirBin){
+    turnServoCW();
+  }
+  else{
+    turnServoCCW();
+  }
+
+  delay(500);*/
+
+  /*if (dirBin){
+    /*extendPiston();
+    Serial.println("pressed");
+    delay(2000);
+    retractPiston();
+    lockServo();
+  }
+
+  Serial.println("it");
+
+  delay(100);*/
+
+bool hasFinishedRunning = false;
+int loopOrderIndex = 0; // 0 is await ball, 1 is for aiming, 2 is for fire ball
+
+const int goalCount = 5;
+const int aimingTiming[] = {0, 100, 300, 200, 300};
+const int aimingDirection[] = {0, 1, 0, 1, 1}; // 1 is to right, 0 is left
+int aimingIndex = 0;
+int aimingTimingCount = 0;
+
 void loop() {
 
-  /*updateDirection();
-  genPWM();*/
+  if (!hasFinishedRunning){
+    if (loopOrderIndex == 0){
+      if (aimingIndex >= goalCount){
+        hasFinishedRunning = true;
+      }
+      else{
+        awaitBall();
+        loopOrderIndex++;
+      }
+    }
+    else if (loopOrderIndex == 1){
+      aimingIteration();
+    }
+    else if (loopOrderIndex == 2){
+      fireBall();
+      loopOrderIndex++;
+    }
+    else{ // reset order
+      loopOrderIndex = 0;
+    }
+  }
+  else{
+    Serial.println("Finished Running");
+    delay(1000);
+  }
 
+}
 
-  delay(500);
+void aimingIteration(){
+  if (aimingIndex < goalCount){
+    if (aimingTimingCount < aimingTiming[aimingIndex]){
+
+      dirBin = aimingDirection[aimingIndex];
+      updateDirection();
+      genPWM();
+
+      aimingTimingCount++;
+
+    }
+    else{
+      aimingIndex++;
+      aimingTimingCount = 0;
+      loopOrderIndex++;
+    }
+  }
+  else{
+    hasFinishedRunning = true;
+  }
+}
+
+void awaitBall(){
+  while (!getSwitchPressed()){
+    Serial.print("awaiting ball for goal #");
+    Serial.println(aimingIndex+1);
+    delay(1);
+  }
+
+  lockServo();
+  delay(1000);
+}
+
+void fireBall(){
+  delay(2000);
+  unlockServo();
+
+  resetPiston();
+  delay(10);
+
+  extendPiston();
+  delay(1000);
+  retractPiston();
+  delay(1000);
 }
 
 // motor controller helper functions
@@ -86,14 +195,6 @@ void updateDirection(){
   // direction change
   if (digitalRead(DIR) != dirBin){
     digitalWrite(DIR, dirBin);
-
-    //
-
-    Serial.print("Direction Change = ");
-    Serial.println(dirBin);
-
-    //
-
     delayMicroseconds(10);
   }
 }
@@ -110,7 +211,7 @@ void genPWM(){
 
 void lockServo(){ // holds servo up against the ball, CW
    turnServoCW();
-   delay(1000);
+   delay(355);
    stopServo();
 }
 
@@ -121,14 +222,17 @@ void unlockServo(){ // gets out of the way of the ball, CCW
 }
 
 void turnServoCW(){
+  //Serial.println("CW");
   updateServoPos(150);
 }
 
 void turnServoCCW(){
+  //Serial.println("CCW");
   updateServoPos(40);
 }
 
 void stopServo(){
+  //Serial.println("Stop");
   updateServoPos(80);
 }
 
@@ -136,4 +240,26 @@ void updateServoPos(int p){
   srv.write(p);
 }
 
+// switch helper functions
+
+int getSwitchPressed(){
+  return 1-digitalRead(MCROSWC);
+}
+
+// pneumatic helper functions
+
+void resetPiston(){
+  digitalWrite(PSTA, LOW);
+  digitalWrite(PSTB, LOW);
+}
+
+void retractPiston(){
+  digitalWrite(PSTA, LOW);
+  digitalWrite(PSTB, HIGH);
+}
+
+void extendPiston(){
+  digitalWrite(PSTA, HIGH);
+  digitalWrite(PSTB, LOW);
+}
 
